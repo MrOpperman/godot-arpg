b4cb8755ddd203838f051fbaef454cf4fa66c6ad
Kyle Opperman <kyleopperman1@gmail.com>
2020-08-02 14:13:11 +0200

    Enemy death effect + bug fixes

--- Stats.gd
+++ Stats.gd
@@ -1,4 +1,11 @@
 extends Node
 
 export var max_health = 1
-onready var health = max_health
+onready var health = max_health setget set_health
+
+signal no_health
+
+func set_health(value):
+	health = value
+	if (value <= 0):
+		emit_signal("no_health")
