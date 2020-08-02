e2a19988d3fba6e895400e4133046cb46e1f2e82
Kyle Opperman <kyleopperman1@gmail.com>
2020-08-02 15:59:22 +0200

    Player Stats + Enemy Attack

--- Enemies/Bat.gd
+++ Enemies/Bat.gd
@@ -20,6 +20,7 @@ var state = CHASE
 onready var sprite = $AnimatedSprite
 onready var stats = $Stats
 onready var playerDetectionZone = $PlayerDetectionZone
+onready var hurtBox = $Hurtbox
 
 func _physics_process(delta):
 	knockback = knockback.move_toward(Vector2.ZERO, FRICTION * delta)
@@ -50,6 +51,7 @@ func _on_Hurtbox_area_entered(area):
 	# this emits the signal 
 	stats.health -= area.damage
 	knockback = area.knockback_vector * 120
+	hurtBox.create_hit_effect()
 	
 func _on_Stats_no_health():
 	queue_free()
--- Player/Player.gd
+++ Player/Player.gd
@@ -14,13 +14,16 @@ enum {
 var state = MOVE
 var velocity = Vector2.ZERO
 var roll_vector = Vector2.DOWN
+var stats = PlayerStats
 
 onready var animationPlayer = $AnimationPlayer
 onready var animationTree = $AnimationTree
 onready var animationState = animationTree.get("parameters/playback")
 onready var swordHitbox = $HitboxPivot/SwordHitbox
+onready var hurtBox = $Hurtbox
 
 func _ready():
+	stats.connect("no_health", self, "queue_free")
 	animationTree.active = true
 	swordHitbox.knockback_vector = roll_vector
 
@@ -79,4 +82,7 @@ func roll_animation_finished():
 func attack_animation_finished():
 	state = MOVE
 
-
+func _on_Hurtbox_area_entered(area):
+	stats.health -= 1
+	hurtBox.start_invincibility(0.5)
+	hurtBox.create_hit_effect()
